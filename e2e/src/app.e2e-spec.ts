79bce43c2c57076f0ea948720dbbf035fdbc9cae
Swetha P <swetha.p@thresholdsoft.com>
2018-05-25 12:10:21 +0530

    second commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('sample-routing App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { CompOneComponent } from './comp-one/comp-one.component';
-import { CompTwoComponent } from './comp-two/comp-two.component';
-import { CompThreeComponent } from './comp-three/comp-three.component';
-import { FirstModule } from './first/first.module';
-import { RouterModule } from '@angular/router';
-import { CompFourComponent } from './comp-four/comp-four.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CompOneComponent,
-    CompTwoComponent,
-    CompThreeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FirstModule,
-    RouterModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
